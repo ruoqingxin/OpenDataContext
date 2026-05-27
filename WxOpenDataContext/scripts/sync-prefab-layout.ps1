1ea09349e88acb9b7ed645b7007ebdbf33e33884
liuwei <halo-2552@outlook.com>
2026-05-27 20:11:34 +0800

    删除实现

--- WxOpenDataContext/scripts/sync-prefab-layout.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = "Stop"
-$ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-node (Join-Path $ScriptDir "sync-prefab-layout.js")
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
